9a700c08a6868bffb1019b3b750952aca82654fd
alexts404 <alexts404dev@gmail.com>
2020-10-09 18:50:18 +0200

    fix: upgrade react-script version to get rid of eslint errors

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+firebase.initializeApp({
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: "kara-628e0.firebaseapp.com",
+    databaseURL: "https://kara-628e0.firebaseio.com",
+    projectId: "kara-628e0",
+    storageBucket: "kara-628e0.appspot.com",
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: "G-X3SZGVYYVG"
+});
 
 ReactDOM.render(
   <React.StrictMode>
