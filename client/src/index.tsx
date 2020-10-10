be4c2aff91d0c2b9879c97b4a9c53519b0592fc0
alexts404 <alexts404dev@gmail.com>
2020-10-10 11:42:07 +0200

    refactor: move firebase set up out of index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,22 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import dotenv from 'dotenv';
 dotenv.config();
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import firebase from 'firebase';
-
-firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
-    databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseio.com`,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: "${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com",
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: `G-${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`
-});
 
 ReactDOM.render(
   <React.StrictMode>
