00f0deea3b5844561f2f1c8d2d6ed539ab42f7c9
alexts404 <alexts404dev@gmail.com>
2020-10-10 15:12:21 +0200

    style: fix styling on firebase

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import styles from './Login.module.scss';
-
-// interface LoginProps {}
+import { useHistory } from 'react-router-dom';
+import { auth, firebase } from '../../services/firebase';
+import Button from '../Button';
 
 const Login: React.FC<unknown> = (props: unknown) => {
-  return <div className={styles.Login}>Login works!</div>;
+  const history = useHistory();
+
+  const googleLogin = async () => {
+    const googleAuth = new firebase.auth.GoogleAuthProvider();
+    await auth
+      .signInWithPopup(googleAuth)
+      .then(async (res) => {
+        const token = await auth?.currentUser?.getIdToken(true);
+        if (token) {
+          sessionStorage.setItem('@token', token);
+          history.push('/');
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  // const emailAuth = new firebase.auth.EmailAuthProvider();
+
+  return (
+    <div className={styles.Login}>
+      <Button>Login</Button>
+    </div>
+  );
 };
 
 export default Login;
--- client/src/services/firebase.ts
+++ client/src/services/firebase.ts
@@ -6,10 +6,10 @@ const firebaseConfig = {
   authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
   databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseio.com`,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: "${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com",
+  storageBucket: '${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com',
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: `G-${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`
+  measurementId: `G-${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`,
 };
 
 firebase.initializeApp(firebaseConfig);
