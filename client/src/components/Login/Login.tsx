430b96c7b555cb6f60e475cf72cca067a619cead
alexts404 <alexts404dev@gmail.com>
2020-10-10 12:40:48 +0200

    feat: add login component

---
+++ client/src/components/Login/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Login.module.scss';
+
+// interface LoginProps {}
+
+const Login: React.FC<unknown> = (props: unknown) => {
+  return <div className={styles.Login}>Login works!</div>;
+};
+
+export default Login;
