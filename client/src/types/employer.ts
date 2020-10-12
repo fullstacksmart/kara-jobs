26491eb383234a65b2dcb2573c92687fa12fd432
pintoedo <eduardoandrespo@gmail.com>
2020-10-12 13:59:44 +0200

    update employer interface

--- client/src/types/employer.ts
+++ client/src/types/employer.ts
@@ -3,4 +3,13 @@ export interface Employer {
   email: string;
   firstName: string;
   lastName: string;
+  companyName: string;
+  sector: string;
+  type: string;
+  street: string;
+  streetNumber: string;
+  addressAditional: string;
+  zipCode: number;
+  city: string;
+  website: string;
 }
