4527c24c3429e598175dfc38ddeb3b79f59ea035
beckertobias <tobias.becker@scalable.capital>
2020-10-12 11:33:00 +0200

    pull from master

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import './App.scss';
 import Router from './containers/Router';
 
-<<<<<<< HEAD
-function App() {
-
-  return <Router />;
-}
-=======
 const App: React.FC<unknown> = () => {
   return (
     <>
@@ -15,6 +9,5 @@ const App: React.FC<unknown> = () => {
     </>
   );
 };
->>>>>>> a6b7a712fab7d3a2a6c35da6df0d01621f17a211
 
 export default App;
