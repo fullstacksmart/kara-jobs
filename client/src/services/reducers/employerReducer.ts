af4076be776978c359ad81a17c26030a2006d287
pintoedo <eduardoandrespo@gmail.com>
2020-10-12 14:12:50 +0200

    update employer reducer

--- client/src/services/reducers/employerReducer.ts
+++ client/src/services/reducers/employerReducer.ts
@@ -6,6 +6,15 @@ const initialState: Employer = {
   email: '',
   firstName: '',
   lastName: '',
+  companyName: string;
+  sector: string;
+  type: string;
+  street: string;
+  streetNumber: string;
+  addressAditional: string;
+  zipCode: number;
+  city: string;
+  website: string;
 };
 
 const EmployerReducer = (
