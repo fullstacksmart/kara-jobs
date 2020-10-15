20da118492978cc67816f4d471501338dfd6a660
alexts404 <alexts404dev@gmail.com>
2020-10-15 16:26:56 +0200

    testing testing

--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,7 @@ import cors from '@koa/cors';
 import dotenv from 'dotenv';
 dotenv.config();
 import router from './routes';
-import db from './db/models/index';
-// the following is needed for the config.js file to be copied to dist/db folder
-// by the ts compiler automatically
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as dbConfig from './db/config/config.js';
+import { db } from './models';
 
 const app = new Koa();
 
@@ -22,7 +18,8 @@ app.use(router.routes()).use(router.allowedMethods());
 
 (async () => {
   try {
-    await db.sequelize.authenticate();
+    db.authenticate();
+    db.sync({ force: true });
     console.log(
       '🔌 connection to db has been established successfully',
     );
--- server/routes.ts
+++ server/routes.ts
@@ -1,10 +1,10 @@
 import Router from '@koa/router';
 
+import * as talentController from './controllers/talents';
+
 const router = new Router();
 
 // TODO: add routes
-router.get('/', (ctx) => {
-  ctx.body = 'getting there';
-});
+router.get('/', talentController.getAll);
 
 export default router;
