54e337b7d15f05ce679dee90548899f2cab2bc4e
beckertobias <tobias.becker@scalable.capital>
2020-10-16 13:58:37 +0200

    change router back to home

--- client/src/containers/Router/Router.tsx
+++ client/src/containers/Router/Router.tsx
@@ -122,7 +122,7 @@ const Router: React.FC<unknown> = () => {
     <Loading />
   ) : (
     <>
-      <Route path="/" exact component={TalentSignUp}></Route>
+      <Route path="/" exact component={Home}></Route>
       <Route path="/signedIn" exact component={Success}></Route>
       <Route path="/sign-in" exact component={LoginContainer}></Route>
       <Route
