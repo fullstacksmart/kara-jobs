8988ffad75f4ef48bafa0f404dfff6af3e7d6229
alexts404 <alexts404dev@gmail.com>
2020-10-19 15:47:00 +0200

    feat: add delete

--- server/controllers/talents.ts
+++ server/controllers/talents.ts
@@ -39,7 +39,7 @@ const subTableNames: SubTableNames[] = Object.keys(
 const fetchTalent = async (
   id: string,
   info: string,
-): Promise<Model | null> => {
+): Promise<Model | Model[] | null> => {
   if (info === 'all') {
     return Talent.findByPk(id, {
       include: [
@@ -97,7 +97,9 @@ const fetchTalent = async (
     for (const subTableName of subTableNames) {
       if (info === subTableName) {
         const table = subTables[subTableName];
-        const result = await table.findByPk.call(table, id);
+        const result = await table.findAll.call(table, {
+          where: { TalentId: id },
+        });
         return result;
       }
     }
@@ -139,16 +141,6 @@ const addTalent = async (
               talentCandidate[subTableName] as TalentArray,
               subTables[subTableName],
             );
-            // await Promise.all(
-            //   (talentCandidate[subTableName] as TalentArray).map(
-            //     async (item) => {
-            //       const newSubtalent = new subTables[subTableName](
-            //         item,
-            //       );
-            //       await newSubtalent.save();
-            //     },
-            //   ),
-            // );
           } else {
             // not an array
             const newSubTalent = new subTables[subTableName](
@@ -248,9 +240,9 @@ export const getAll = async (ctx: Context): Promise<void> => {
   ctx.body = allTalents;
 };
 
-export const addOne = async (ctx: Context): Promise<void> => {
+export const addForSignup = async (ctx: Context): Promise<void> => {
   const id = ctx.params.id;
-  ctx.status = 401;
+  ctx.status = 400;
   try {
     const existingTalent = await Talent.findByPk(id);
     // TODO: rethink/discuss onboarding update logic
@@ -266,12 +258,19 @@ export const addOne = async (ctx: Context): Promise<void> => {
         existingTalent &&
         !existingTalent.onboardingComplete
       ) {
-        const updatedTalent = await updateTalent(
-          existingTalent,
-          talentCandidate,
-        );
-        ctx.status = 201;
-        ctx.body = updatedTalent;
+        try {
+          const updatedTalent = await updateTalent(
+            existingTalent,
+            talentCandidate,
+          );
+          ctx.status = 201;
+          ctx.body = updatedTalent;
+        } catch (err) {
+          ctx.status = 400;
+          const message = `Talent does not have the right format to be included in db:\n${err}`;
+          ctx.body = message;
+          console.error(message);
+        }
         return;
       } else {
         try {
@@ -283,7 +282,7 @@ export const addOne = async (ctx: Context): Promise<void> => {
           ctx.body = newTalent;
           return;
         } catch (err) {
-          ctx.status = 401;
+          ctx.status = 400;
           ctx.body = `Talent does not have the right format to be included in db:\n${err}`;
           return;
         }
@@ -291,5 +290,197 @@ export const addOne = async (ctx: Context): Promise<void> => {
     }
   } catch (err) {
     ctx.status = 500;
+  }
+};
+
+export const addOne = async (ctx: Context): Promise<void> => {
+  const id = ctx.params.id;
+  const type = ctx.params.type || 'signup';
+  const talentCandidate = ctx.request.body;
+  try {
+    if (type === 'signup') {
+      return addForSignup(ctx);
+    }
+    for (const subTableName of subTableNames) {
+      if (type === subTableName) {
+        const currentTable = subTables[subTableName];
+        const existingEntry = await currentTable.findAll.call(
+          currentTable,
+          {
+            where: {
+              TalentId: id,
+            },
+          },
+        );
+        if (existingEntry && !Array.isArray(existingEntry)) {
+          ctx.status = 400;
+          ctx.body = `info of type ${type} already exists for talent ${id}`;
+          return;
+        } else {
+          try {
+            let addedTable;
+            if (Array.isArray(talentCandidate)) {
+              addedTable = await currentTable.bulkCreate.call(
+                currentTable,
+                talentCandidate,
+              );
+            } else {
+              const newSubtable = new currentTable(talentCandidate);
+              addedTable = await newSubtable.save();
+            }
+            ctx.status = 201;
+            ctx.body = addedTable;
+          } catch (err) {
+            ctx.status = 400;
+            const message = `Error inserting into db: ${err}`;
+            ctx.body = message;
+            console.error(message);
+          }
+        }
+      }
+    }
+  } catch (err) {
+    ctx.status = 500;
+    console.error(err);
+  }
+};
+
+export const updateOne = async (ctx: Context): Promise<void> => {
+  const type = ctx.params.type || 'all';
+  const id = ctx.params.id;
+  ctx.status = 400;
+  try {
+    const existingTalent = await Talent.findByPk(id);
+    if (!existingTalent) {
+      const message = `talent with id ${id} does not exist`;
+      ctx.body = message;
+      return;
+    }
+    if (type === 'all') {
+      try {
+        const updatedTalent = await updateTalent(
+          existingTalent,
+          ctx.request.body,
+        );
+        ctx.status = 200;
+        ctx.body = updatedTalent;
+      } catch (err) {
+        ctx.status = 400;
+        const message = `invalid format.\n${err}`;
+        ctx.body = message;
+        console.error(message);
+      }
+    } else {
+      for (const subTableName of subTableNames) {
+        if (type === subTableName) {
+          const currentTable = subTables[subTableName];
+          const updateInfo = ctx.request.body;
+          let updatedItem: any;
+          try {
+            if (Array.isArray(updateInfo)) {
+              updatedItem = await Promise.all(
+                updateInfo.map(async (item) => {
+                  await currentTable.update.call(currentTable, item, {
+                    where: {
+                      TalentId: id,
+                      id: item.id,
+                    },
+                  });
+                  const currentUpdate = await currentTable.findByPk.call(
+                    currentTable,
+                    item.id,
+                  );
+                  return currentUpdate;
+                }),
+              );
+            } else {
+              // not an array
+              await currentTable.update.call(
+                currentTable,
+                updateInfo,
+                {
+                  where: {
+                    TalentId: id,
+                  },
+                },
+              );
+              updatedItem = await currentTable.findAll.call(
+                currentTable,
+                {
+                  where: { TalentId: id },
+                },
+              );
+            }
+            ctx.status = 200;
+            ctx.body = updatedItem;
+          } catch (err) {
+            ctx.status = 400;
+            const message = `invalid format: ${err}`;
+            ctx.body = message;
+            console.error(message);
+          }
+        }
+      }
+      return;
+    }
+    ctx.status = 400;
+    ctx.body(`invalid request. ${type} is not a valid type.`);
+  } catch (err) {
+    ctx.status = 500;
+  }
+};
+
+export const deleteOne = async (ctx: Context): Promise<void> => {
+  const id = ctx.params.id;
+  const type = ctx.params.type || 'all';
+  const existingTalent = await Talent.findByPk(id);
+  if (!existingTalent) {
+    ctx.status = 400;
+    ctx.body = `No talent with id ${id} in db`;
+    return;
+  }
+  if (type === 'all') {
+    try {
+      Talent.destroy({
+        where: {
+          id,
+        },
+      });
+      ctx.status = 204;
+    } catch (err) {
+      ctx.status = 500;
+    }
+  } else {
+    for (const subTableName of subTableNames) {
+      if (type === subTableName) {
+        const typeId = ctx.params.typeId;
+        const currentTable = subTables[subTableName];
+        if (typeId) {
+          try {
+            currentTable.destroy.call(currentTable, {
+              where: {
+                id: typeId,
+              },
+            });
+            ctx.status = 204;
+            return;
+          } catch (err) {
+            ctx.status = 500;
+          }
+        } else {
+          try {
+            currentTable.destroy.call(currentTable, {
+              where: {
+                TalentId: id,
+              },
+            });
+            ctx.status = 204;
+            return;
+          } catch (err) {
+            ctx.status = 500;
+          }
+        }
+      }
+    }
   }
 };
--- server/routes.ts
+++ server/routes.ts
@@ -13,4 +13,19 @@ router.get('/talents/:id/:type', talentController.getOne);
 
 router.post('/talents/:id', talentController.addOne);
 
+router.post('/talents/:id/:type', talentController.addOne);
+
+router.put('/talents/:id', talentController.updateOne);
+
+router.put('/talents/:id/:type', talentController.updateOne);
+
+router.delete('/talents/:id', talentController.deleteOne);
+
+router.delete('/talents/:id/:type', talentController.deleteOne);
+
+router.delete(
+  '/talents/:id/:type/:typeId',
+  talentController.deleteOne,
+);
+
 export default router;
