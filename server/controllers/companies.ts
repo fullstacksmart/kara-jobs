e28f1bac66c6ab2f41cf9619a414077d79e68367
alexts404 <alexts404dev@gmail.com>
2020-10-19 20:30:50 +0200

    feat: add addOne

--- server/controllers/companies.ts
+++ server/controllers/companies.ts
@@ -5,6 +5,8 @@ import { CompanyImage } from '../models/Company/CompanyImage';
 import { CompanySearchPreferences } from '../models/Company/CompanySearchPreferences';
 import { CompanyRecruitmentPreferences } from '../models/Company/CompanyRecruitmentPreferences';
 import { CompanyProfile } from '../models/Company/CompanyProfile';
+import { Model } from 'sequelize-typescript';
+import { ModelStatic } from 'sequelize/types';
 
 const subTables = {
   employees: CompanyEmployee,
@@ -97,11 +99,112 @@ export const getOneFromEmployee = async (
   ctx: Context,
 ): Promise<void> => {
   const employeeId = ctx.params.employeeId;
-  const employee = await CompanyEmployee.findByPk(employeeId);
-  const companyId = employee?.CompanyId;
-  if (companyId) {
-    getOne(ctx, companyId);
-  } else {
-    ctx.status = 404;
+  try {
+    const employee = await CompanyEmployee.findByPk(employeeId);
+    const companyId = employee?.CompanyId;
+    if (companyId) {
+      getOne(ctx, companyId);
+    } else {
+      ctx.status = 404;
+    }
+  } catch (err) {
+    ctx.status = 500;
   }
+};
+
+const addOrUpdateSubtable = async (
+  companyTable: ModelStatic<Model>,
+  value: any,
+  id: string,
+) => {
+  const [signingUpCompany, created] = await Model.findOrCreate.call(
+    companyTable,
+    {
+      where: {
+        CompanyId: id,
+      },
+      defaults: value,
+    },
+  );
+  if (!created) {
+    signingUpCompany.update(value);
+  }
+};
+
+export const addOne = async (
+  ctx: Context,
+  id?: string,
+): Promise<void> => {
+  id = id || ctx.params.id;
+  let fromDb;
+  let dbErr;
+  const type = ctx.params.type;
+  const companyCandidate = ctx.request.body;
+  if (!companyCandidate) {
+    ctx.status = 400;
+    return;
+  }
+  if (type === 'all') {
+    try {
+      fromDb = Company.create(companyCandidate);
+    } catch (err) {
+      dbErr = err;
+    }
+  } else if (type === 'signup') {
+    try {
+      const [
+        signingUpCompany,
+        created,
+      ] = await Model.findOrCreate.call(Company, {
+        where: {
+          id: id,
+        },
+        defaults: companyCandidate,
+      });
+      if (!created) {
+        signingUpCompany.update(companyCandidate);
+      }
+      fromDb = signingUpCompany;
+    } catch (err) {
+      dbErr = err;
+    }
+  } else {
+    for (const subTableName of subTableNames) {
+      if (type === subTableName) {
+        const currentSubTable = subTables[subTableName];
+        if (Array.isArray(companyCandidate)) {
+          try {
+            fromDb = await Promise.all(
+              companyCandidate.map(async (item) => {
+                return await addOrUpdateSubtable(
+                  currentSubTable,
+                  item,
+                  id as string,
+                );
+              }),
+            );
+          } catch (err) {
+            dbErr = err;
+          }
+        } else {
+          try {
+            fromDb = await addOrUpdateSubtable(
+              currentSubTable,
+              companyCandidate,
+              id as string,
+            );
+          } catch (err) {
+            dbErr = err;
+          }
+        }
+      }
+    }
+  }
+  if (dbErr !== undefined) {
+    ctx.status = 400;
+    ctx.body = dbErr;
+    return;
+  }
+  ctx.status = 201;
+  ctx.body = fromDb;
 };
