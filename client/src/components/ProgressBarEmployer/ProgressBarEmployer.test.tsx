d577e871840853e35b0340a903816bf86772fc87
camifernweh <camiladmuller@gmail.com>
2020-10-21 23:55:15 +0200

    stle: ProgressBarEmployer and EmployerSignUp1

---
+++ client/src/components/ProgressBarEmployer/ProgressBarEmployer.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+// import ProgressBarEmployer from './ProgressBarEmployer';
+// describe('ProgressBarEmployer', () => {
+//   it('renders ProgressBarEmployer', () => {
+//     render(<ProgressBarEmployer />);
+//     expect(screen.getByText('ProgressBarEmployer works!')).not.toBe(null);
+//   })
+// })
---
+++ client/src/components/ProgressBarEmployer/index.ts
@@ -0,0 +1,3 @@
+import ProgressBarEmployer from './ProgressBarEmployer';
+
+export default ProgressBarEmployer;
