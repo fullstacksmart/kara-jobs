29bd81f35d671c63b94daaafbcc2550c92cfcc3b
alexts404 <alexts404dev@gmail.com>
2020-10-21 22:30:37 +0200

    feat: add Array Frame Component

---
+++ client/src/components/ArrayFrame/ArrayFrame.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+// import ArrayFrame from './ArrayFrame';
+describe('ArrayFrame', () => {
+  it('renders ArrayFrame', () => {
+    render(<div></div>);
+    // render(<ArrayFrame />);
+    // expect(screen.getByText('')).not.toBe(null);
+  });
+});
---
+++ client/src/components/ArrayFrame/ArrayFrame.tsx
@@ -0,0 +1,38 @@
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import styles from './ArrayFrame.module.scss';
+
+interface ArrayFrameProps {
+  items?: React.ReactNode[];
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  header: string;
+}
+
+const ArrayFrame: React.FC<ArrayFrameProps> = ({
+  header,
+  items,
+  onClick,
+}: ArrayFrameProps) => {
+  const list =
+    items?.map((item) => {
+      <div className={styles.item}>{item}</div>;
+    }) || [];
+  return (
+    <div className={styles.ArrayFrame}>
+      <div className={styles.HeaderInset}>
+        <h1>{header}</h1>
+        <div onClick={onClick}>
+          <FontAwesomeIcon
+            icon={faPlusCircle}
+            size="2x"
+            className={styles.PlusButton}
+          />
+        </div>
+      </div>
+      {list.length ? <div className={styles.Border}>{list}</div> : <></>}
+    </div>
+  );
+};
+
+export default ArrayFrame;
