a2d6aeea677d6d3f89d74bf66de6193533913418
alexts404 <alexts404dev@gmail.com>
2020-10-22 00:48:24 +0200

    feat: add Talent mocks to main profile

--- client/src/containers/ProfileMain/ProfileMain.tsx
+++ client/src/containers/ProfileMain/ProfileMain.tsx
@@ -3,6 +3,42 @@ import styles from './ProfileMain.module.scss';
 import MainInfo from '../../components/MainInfo';
 import AboutMe from '../../components/AboutMe';
 import ArrayFrame from '../../components/ArrayFrame';
+import TalentExperience from '../../components/TalentExperience';
+import { Experience } from '../../types/talent';
+
+// mocks
+
+const experiences: Experience[] = [
+  {
+    id: 1,
+    TalentId: 'abcd',
+    // 1: ''
+    // 2:
+    // 3:
+    occupationId: 2,
+    positionName: 'Krankenpflegerin',
+    // 0:
+    // 1:
+    // 2:
+    occupationStatusId: 0,
+    employerName: 'Schwarzwaldklinik',
+    employerCity: 'Hintertupfingen',
+    positionStartMonth: 11,
+    positionStartYear: 2019,
+  },
+  {
+    id: 2,
+    TalentId: 'qzb',
+    occupationId: 1,
+    positionName: 'Chefärztin',
+    occupationStatusId: 1,
+    employerName: 'Praxis Bülowbogen',
+    positionStartMonth: 1,
+    positionStartYear: 2015,
+    positionEndMonth: 1,
+    positionEndYear: 2016,
+  },
+];
 
 const ProfileMain: React.FC<unknown> = (props: unknown) => {
   return (
@@ -15,7 +51,7 @@ const ProfileMain: React.FC<unknown> = (props: unknown) => {
         country="Serbien"
       />
       <AboutMe title="Über Dich" />
-      <ArrayFrame items={[]} header="Deine Erfahrung" />
+      <TalentExperience experiences={experiences} />
       <ArrayFrame items={[]} header="Deine Ausbildung" />
     </div>
   );
