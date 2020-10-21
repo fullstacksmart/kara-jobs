e85812e293311b12b6abcfd62daa3e4b33aba12a
alexts404 <alexts404dev@gmail.com>
2020-10-21 19:34:33 +0200

    feat: add profilepicture select popup to main Info

--- client/src/components/MainInfo/MainInfo.tsx
+++ client/src/components/MainInfo/MainInfo.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import Button from '../Button';
+import EditInfo from '../EditInfo';
+import PicEdit from '../PicEdit';
 import styles from './MainInfo.module.scss';
 
 interface MainInfoAttributes {
@@ -12,14 +17,29 @@ const MainInfo: React.FC<MainInfoAttributes> = ({
   profession,
   city,
 }: MainInfoAttributes) => {
+  const handlePicClick = (event: React.MouseEvent) => {
+    console.log('clicked');
+  };
+
+  const [showPicEdit, setShowPicEdit] = useState(false);
+  const [showInfoEdit, setShowInfoEdit] = useState(false);
+
   return (
     <div className={styles.MainInfo}>
-      <div className={styles.ProfilePic}></div>
+      <div className={styles.PicContainer}>
+        <div className={styles.ProfilePic}></div>
+        <EditInfo onClick={() => setShowPicEdit(true)} />
+      </div>
       <div className={styles.InfoText}>
         <h1>{name}</h1>
-        <h2>{profession}</h2>
-        <h2>{city}</h2>
+        <div className={styles.secondaryText}>
+          <h2>{profession}</h2>
+          <h2>{city}</h2>
+        </div>
       </div>
+      <EditInfo top="0" />
+      {showPicEdit ? <PicEdit setShowPicEdit={setShowPicEdit} /> : ''}
+      {/*showInfoEdit ? <InfoEdit setShowPicEdit={setShowInfoEdit} /> : ''*/}
     </div>
   );
 };
