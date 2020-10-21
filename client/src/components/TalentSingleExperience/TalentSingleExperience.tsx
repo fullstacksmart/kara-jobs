709b5744bb6084b44824cbfeb2bda0dc42456c19
alexts404 <alexts404dev@gmail.com>
2020-10-22 00:47:33 +0200

    feat: add TalentSingleExperience component

---
+++ client/src/components/TalentSingleExperience/TalentSingleExperience.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { getDuration } from '../../services/dateService';
+import { Experience } from '../../types/talent';
+import EditInfo from '../EditInfo';
+import TextInfo from '../TextInfo';
+import styles from './TalentSingleExperience.module.scss';
+
+interface TalentSingleExperienceProps {
+  experience: Experience;
+}
+
+const TalentSingleExperience: React.FC<TalentSingleExperienceProps> = ({
+  experience,
+}: TalentSingleExperienceProps) => {
+  const durationString = getDuration(
+    experience.positionStartMonth,
+    experience.positionStartYear,
+    experience.positionEndMonth,
+    experience.positionEndYear,
+  );
+
+  return (
+    <div className={styles.TalentSingleExperience}>
+      <div className={(styles.Half, styles.LeftHalf)}>
+        <TextInfo title="Position" info={experience.positionName} />
+        <TextInfo
+          title="Arbeitgeber/Unternehmen"
+          info={experience.employerName}
+        />
+        {experience.employerCity ? (
+          <TextInfo title="Ort" info={experience.employerCity} />
+        ) : (
+          <></>
+        )}
+        {experience.positionDescription ? (
+          <TextInfo
+            title="Beschreibung"
+            info={experience.positionDescription}
+          />
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className={styles.Half}>
+        <div className={styles.EditContainer}>
+          <EditInfo top="0" />
+        </div>
+        {durationString}
+      </div>
+    </div>
+  );
+};
+
+export default TalentSingleExperience;
